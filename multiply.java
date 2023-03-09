d1c2c21fe00a9c1fdb8fc1adf122ed3ef9616fe6
Brijesh Soni <brijeshsoni98245@gmail.com>
2023-03-09 20:21:06 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args){
+        int a =10;
+        int b = 12;
+        System.out.println("the multiplication is: "+(a*b));
+    }
+    
+}
