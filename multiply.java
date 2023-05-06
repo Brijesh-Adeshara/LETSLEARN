beb8fc6d86e35843ff5f43db4ff55508d8783a23
Brijesh Soni <127421200+Brijesh-Adeshara@users.noreply.github.com>
2023-05-06 20:46:18 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,6 +1,6 @@
 public class multiply {
     public static void main(String[] args){
-        int a =10;
+        int a =20;
         int b = 12;
         System.out.println("the multiplication is: "+(a*b));
     }
